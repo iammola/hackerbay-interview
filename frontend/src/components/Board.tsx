8357f3bbf696d152097bb128708959e3b650414b
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-20 09:04:47 +0100

    dev(frontend): generate player & enemies position :construction:

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -1,8 +1,23 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useLayoutEffect, useState } from "react";
 
 import Tile from "./Tile";
 
 const Board: FunctionComponent<BoardProps> = ({ height, width }) => {
+  const [player, setPlayer] = useState<number>();
+  const [enemies, setEnemies] = useState<number[]>([]);
+
+  useLayoutEffect(() => {
+    const getRandomPoint = () => Math.floor(Math.random() * height * width);
+
+    let player = getRandomPoint();
+    const enemies = new Array((height + width) / 2).fill(0).map(getRandomPoint);
+
+    while (enemies.includes(player)) player = getRandomPoint();
+
+    setPlayer(player);
+    setEnemies(enemies);
+  }, [height, width]);
+
   return (
     <div
       className="grid w-max border border-slate-600"
@@ -12,7 +27,10 @@ const Board: FunctionComponent<BoardProps> = ({ height, width }) => {
       }}
     >
       {new Array(width * height).fill(null).map((_, i) => (
-        <Tile key={i} />
+        <Tile key={i}>
+          {player === i && "P"}
+          {enemies.includes(i) && "E"}
+        </Tile>
       ))}
     </div>
   );
