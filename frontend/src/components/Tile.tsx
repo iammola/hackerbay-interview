abf7ca18123fbdf4b184fbef0bb88fa3e64b7816
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-20 11:44:13 +0100

    feat(frontend): increase tile size :art:

--- frontend/src/components/Tile.tsx
+++ frontend/src/components/Tile.tsx
@@ -2,7 +2,7 @@ import { FunctionComponent } from "react";
 
 const Tile: FunctionComponent = ({ children }) => {
   return (
-    <div className="flex h-12 w-12 items-center justify-center border p-2 [border-color:inherit]">
+    <div className="flex h-16 w-16 items-center justify-center border p-1 [border-color:inherit]">
       {children}
     </div>
   );
