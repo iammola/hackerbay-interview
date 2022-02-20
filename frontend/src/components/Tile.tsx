b9d199d6a76a3a09e92d37c8056fe783a306f85b
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-20 08:39:13 +0100

    dev(frontend): add Tile styles :construction:

--- frontend/src/components/Tile.tsx
+++ frontend/src/components/Tile.tsx
@@ -1,7 +1,11 @@
 import { FunctionComponent } from "react";
 
-const Tile: FunctionComponent = () => {
-  return <></>;
+const Tile: FunctionComponent = ({ children }) => {
+  return (
+    <div className="flex h-12 w-12 items-center justify-center border p-2 [border-color:inherit]">
+      {children}
+    </div>
+  );
 };
 
 export default Tile;
