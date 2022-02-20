5223f28a120ebfc1c2080eff79b9186a0380baae
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-20 07:08:02 +0100

    initial(frontend): create Board component :tada:

---
+++ frontend/src/components/Board.tsx
@@ -0,0 +1,12 @@
+import { FunctionComponent } from "react";
+
+const Board: FunctionComponent<BoardProps> = () => {
+  return <></>;
+};
+
+export default Board;
+
+type BoardProps = {
+  width: number;
+  height: number;
+};
