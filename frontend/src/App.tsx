7bce748bfa54a674ee8e634acfa5ded38c9e887e
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-20 07:43:12 +0100

    dev(frontend): add App :construction:

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,24 @@
-function App() {
-  return <></>;
-}
+import { FunctionComponent, useEffect, useState } from "react";
+
+import Board from "./components/Board";
+
+const App: FunctionComponent = () => {
+  const [size, setSize] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    if (!size.width)
+      setSize((size) => ({
+        ...size,
+        width: +(prompt("Please enter board width", "10") ?? 0),
+      }));
+    if (!size.height)
+      setSize((size) => ({
+        ...size,
+        height: +(prompt("Please enter board height", "10") ?? 0),
+      }));
+  }, [size]);
+
+  return <Board {...size} />;
+};
 
 export default App;
