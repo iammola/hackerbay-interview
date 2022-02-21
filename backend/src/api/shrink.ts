5c0caa73c71019f84e4858b816ee9aa635b6efb3
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-21 09:04:28 +0100

    dev(backend): add `shrinkImage` handler :construction:

--- backend/src/api/shrink.ts
+++ backend/src/api/shrink.ts
@@ -1,3 +1,5 @@
+import { got } from "got";
+import sharp from "sharp";
 import { StatusCodes } from "http-status-codes";
 
 import { routeWrapper, validateJWT } from "../utils";
@@ -9,6 +11,17 @@ export const shrinkImage = (req: Request, res: Response) =>
 
 async function handler(req: Request, res: Response) {
   await validateJWT(req);
+  if (typeof req.body !== "string") throw new Error("Invalid Body");
+
+  const { url } = JSON.parse(req.body) as { url: string };
+  if (!url) throw new Error("Invalid image URL");
+
+  const resizer = sharp().resize(50, 50).toFormat("webp");
+  got
+    .stream(url)
+    .pipe(resizer)
+    .pipe(res)
+    .on("close", () => res.end());
 
   res.status(StatusCodes.OK);
 }
