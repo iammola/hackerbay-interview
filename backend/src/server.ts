63786af86b9d111d22bf41f452a8306720d2056a
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-21 11:01:45 +0100

    feat(backend): add trailing slash to api routes :sparkles:

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,6 +6,6 @@ import { authUser, patchBody, shrinkImage } from "./api";
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 const server = createServer().use(parse).use(plugins.bodyParser());
 server.listen(8125);
-server.post("/api/auth", authUser);
-server.post("/api/patch", patchBody);
-server.post("/api/shrink", shrinkImage);
+server.post("/api/auth/", authUser);
+server.post("/api/patch/", patchBody);
+server.post("/api/shrink/", shrinkImage);
