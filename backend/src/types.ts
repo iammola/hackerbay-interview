5096df79fdbcab0a933342deee41c7fd16f6c647
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-22 09:56:33 +0100

    enh(types): specify operations types :zap:

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -8,11 +8,32 @@ export type PatchRequestBody = {
 export type PatchObject = {
   /** Path to property */
   path: string;
-  /** Value to replace */
-  value: string;
-  /** Operation to run */
-  op: "add" | "remove" | "replace" | "move" | "copy" | "test";
-};
+} & (
+  | {
+      /** Operation to run */
+      op: "add" | "replace" | "test";
+      /** Has no effect on these operations */
+      from?: never;
+      /** Value to replace */
+      value: string;
+    }
+  | {
+      /** Operation to run */
+      op: "remove";
+      /** Has no effect on this operation */
+      from?: never;
+      /** Has no effect on this operation */
+      value?: never;
+    }
+  | {
+      /** Operation to run */
+      op: "copy" | "move";
+      /** Where to copy or move from */
+      from: string;
+      /** Has no effect on these operation */
+      value?: never;
+    }
+);
 
 export type AuthRequestBody = {
   /** Username */
